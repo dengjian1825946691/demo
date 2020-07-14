e7414c20f831eddcdc1a868972bb15df1f8378a3
dengjian <1825946691@qq.com>
2020-07-14 12:21:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello(){
+        return "hell0 woefesn!";
+    }
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+}
